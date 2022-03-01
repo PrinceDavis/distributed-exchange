94c2422172a2652197d2bfdbac859827e74d09fc
ThankGod Ossaija (Tg) <codebugsolved@gmail.com>
2022-03-01 15:29:01 +0100

    ready to push

--- client.js
+++ client.js
@@ -1,6 +1,11 @@
 'use strict'
 const { PeerRPCClient }  = require('grenache-nodejs-ws')
 const Link = require('grenache-nodejs-link')
+const { v4: uuidV4 } = require('uuid')
+
+const { OrderBook } = require('./order-book')
+const { Order, OrderTypes } = require('./order')
+
 const link = new Link({
   grape: `http://${process.env.HOST}:${process.env.LINK_PORT}`,
   requestTimeout: 10000
@@ -9,10 +14,46 @@ link.start()
 const peer = new PeerRPCClient(link, {})
 peer.init()
 
+/** create own order */
+const orderBook = new OrderBook()
 const quantity = Math.floor(Math.random() * 100) + 1;
 const price = Math.floor(Math.random() * 11) + 1
-const payload = { quantity, price, symbol: 'tBTCUSD' }
+const myClientId = uuidV4()
+
+const payload = { quantity, price, symbol: 'tBTCUSD', clientId: myClientId, type: OrderTypes.LIMITE_ORDER }
+
+const { price, quantity, type, symbol } = payload
+const order = new Order({
+  timestamp: Date().now(),
+  quantity,
+  symbol,
+  price
+})
+
+if(type === OrderTypes.LIMITE_ORDER) {
+  orderBook.processLimitOrder(order)
+}else if(type === OrderTypes.MARKET_ODER) {
+  orderBook.processMarketOrder()
+}
+
+
 peer.request('orderbook_worker', payload, { timeout: 100000 }, (err, result) => {
   if (err) throw err
-  console.log( result)
+
+ const { price, quantity, type, symbol, clientId  } = result
+ if(clientId !== myClientId) {
+  const order = new Order({
+    timestamp: Date().now(),
+    quantity,
+    symbol,
+    price
+  })
+  if(type === OrderTypes.LIMITE_ORDER) {
+    result = orderBook.processLimitOrder(order)
+  }else if(type === OrderTypes.MARKET_ODER) {
+    result = orderBook.processMarketOrder()
+  }else {
+    error = new Error('order type does not exist')
+  }
+ }
 })
--- server.js
+++ server.js
@@ -3,8 +3,6 @@ require('dotenv').config()
 const { PeerRPCServer } = require('grenache-nodejs-ws')
 const Link = require('grenache-nodejs-link')
 
-const { OrderBook } = require('./order-book')
-const { Order, OrderTypes } = require('./order')
 
 const link = new Link({
   grape: `http://${process.env.HOST}:${process.env.LINK_PORT}`
@@ -21,26 +19,7 @@ setInterval(() => {
 
 }, 1000)
 
-const orderBook = new OrderBook()
 service.on('request', (rid, key, payload, handler) => {
-  const { price, quantity, type, symbol } = payload
-  const order = new Order({
-    timestamp: Date().now(),
-    quantity,
-    symbol,
-    price
-  })
-let result = null
-let error = null
-
-  if(type === OrderTypes.LIMITE_ORDER) {
-    result = orderBook.processLimitOrder(order)
-  }else if(type === OrderTypes.MARKET_ODER) {
-    result = orderBook.processMarketOrder()
-  }else {
-    error = new Error('order type does not exist')
-  }
-
-  const result = fibonacci(payload.number)
-  handler.reply(error, result)
+  // send request to all clients
+  handler.reply(error, payload)
 })
